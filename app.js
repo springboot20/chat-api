0d5b8af716964ee20c3fd715403ae8731c743981
Yunus Abbas Opeyemi <opeyemiakanbi328@gmail.com>
2024-07-26 16:45:57 +0100

    updated

--- app.js
+++ app.js
@@ -67,9 +67,9 @@ app.use("/api/v1/chat-app/chats", chatRouter);
 app.use("/api/v1/chat-app/messages", messageRouter);
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", process.env.CORS_ORIGIN);
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Headers", "*");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
 });
 
