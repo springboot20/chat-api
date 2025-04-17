2928e34bd5de4a59bdee3adfe8fa8175c3ee15e5
springboot20 <abbasopeyemi148@gmail.com>
2025-04-17 11:49:20 +0100

    fixed cors

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(
   cors({
-    origin: true,
+    origin: process.env.CORS_ORIGIN,
     // credentials: true,
   })
 );
@@ -61,7 +61,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", process.env.CORS_ORIGIN);
   res.setHeader("Access-Control-Allow-Methods", "*");
   res.setHeader("Access-Control-Allow-Headers", "*");
 
