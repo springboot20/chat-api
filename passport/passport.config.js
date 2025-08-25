a1f596c43a598154e854df89f32fb3cbc3377494
springboot20 <abbasopeyemi148@gmail.com>
2025-08-25 01:43:31 +0100

    refactor: standardize string formatting and improve code readability in passport configuration

--- passport/passport.config.js
+++ passport/passport.config.js
@@ -1,9 +1,9 @@
-import { Strategy as LocalStrategy } from 'passport-local';
-import passport from 'passport';
-import { userModel } from '../models/index.js';
-import { LoginType } from '../constants/constants';
-import { ApiError } from '../utils/ApiError.js';
-import { StatusCodes } from 'http-status-codes';
+import { Strategy as LocalStrategy } from "passport-local";
+import passport from "passport";
+import { userModel } from "../models/index.js";
+import { LoginType } from "../constants/constants";
+import { ApiError } from "../utils/ApiError.js";
+import { StatusCodes } from "http-status-codes";
 
 passport.use(
   new LocalStrategy(async (username, email, password, next) => {
@@ -16,46 +16,50 @@ passport.use(
               400,
               `You have previously registered using ${user?.loginType
                 .toLowerCase()
-                .split('_')
-                .join(' ')}. Please use the ${user?.loginType
+                .split("_")
+                .join(" ")}. Please use the ${user?.loginType
                 .toLowerCase()
-                .split('_')
-                .join(' ')} login option to access your account`
+                .split("_")
+                .join(" ")} login option to access your account`
             ),
             null
           );
         }
         const isValidPassword = await user?.isPasswordCorrect(password);
         if (!isValidPassword) {
-          next(new ApiError(409, 'Invalid password, please check your password'), null);
+          next(new ApiError(409, "Invalid password, please check your password"), null);
         }
 
         return next(null, user);
       })
       .catch((error) => {
-        next(new ApiError(409, 'Something went wrong while registering user'), null);
+        next(new ApiError(409, "Something went wrong while registering user"), null);
       });
   })
 );
 
 passport.serializeUser((user, next) => {
-  next(null, user?._id);
+  process.nextTick(function () {
+    next(null, user?._id);
+  });
 });
 
-passport.deserializeUser(async (id, next) => {
-  await userModel
-    .findById(id)
-    .then((user) => {
-      if (user) next(null, user);
-      else next(new ApiError(StatusCodes.NOT_FOUND, 'User does not exist'), null);
-    })
-    .catch((error) => {
-      next(
-        new ApiError(
-          StatusCodes.INTERNAL_SERVER_ERROR,
-          `Something went wrong while trying to serialize user. Error : ${error}`
-        ),
-        null
-      );
-    });
+passport.deserializeUser((id, next) => {
+  process.nextTick(async function () {
+    await userModel
+      .findById(id)
+      .then((user) => {
+        if (user) next(null, user);
+        else next(new ApiError(StatusCodes.NOT_FOUND, "User does not exist"), null);
+      })
+      .catch((error) => {
+        next(
+          new ApiError(
+            StatusCodes.INTERNAL_SERVER_ERROR,
+            `Something went wrong while trying to serialize user. Error : ${error}`
+          ),
+          null
+        );
+      });
+  });
 });
